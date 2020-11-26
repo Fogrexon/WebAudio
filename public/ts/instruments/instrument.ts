3f9d27f37d65bfa8a5d23b2f51b6dfe3a26c5b2e
Fogrexon <fogrexon@gmail.com>
2020-11-26 13:14:29 +0900

    music insert

--- public/ts/instruments/instrument.ts
+++ public/ts/instruments/instrument.ts
@@ -5,7 +5,7 @@ const doremi = [0, 2, 4, 5, 7, 9, 11];
 const sharp = [1, 3, 4, 6, 8, 10, 11];
 
 const notesGenerator = (_base: number) => {
-  const notes = range(-12, 24).map((num) => _base * pow12 ** num);
+  const notes = range(-12, 36).map((num) => _base * pow12 ** num);
   return (note) => {
     const oct = Math.floor(note / 7);
     const index = (() => {
@@ -34,8 +34,14 @@ export default class Instrument {
   }
 
   play(index: number, length: number): Promise<void> {
+    if (index < -7 || index >= 21) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, (60000 / this.bpm) * length);
+      });
+    }
     const oscillator = this.context.createOscillator();
-    console.log(this.notes(index));
     oscillator.frequency.value = this.notes(index);
     const gain = this.context.createGain();
     gain.gain.value = this.volume;
--- public/ts/main.ts
+++ public/ts/main.ts
@@ -1,13 +1,15 @@
 /* eslint-disable array-callback-return */
 import Instrument from './instruments/instrument';
-import { range } from './utils';
+import * as musicData from './music';
 
 const inst = new Instrument(261.626, 120);
 inst.volume = 0.2;
 
 document.getElementById('button').addEventListener('click', () => {
-  let chain = inst.play(-7, 0);
-  range(-7, 14).map((index) => {
-    chain = chain.then(() => inst.play(index, 1));
+  let chain = new Promise((resolve) => {
+    setTimeout(() => resolve());
+  });
+  musicData.notes.map((_, index) => {
+    chain = chain.then(() => inst.play(musicData.notes[index], musicData.length[index]));
   });
 });
